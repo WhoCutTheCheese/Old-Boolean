19b812ec412bb2bfd23b78cf3cab8fa1dc659b47
WhoCutTheCheese <aidenconway07@gmail.com>
2022-10-31 07:10:48 -0500

    Fixed Mod Logs
    Fixed a big with mod logs trying to send a message in a channel is can't see
    Fixed a bug with `!!warn` warning and muting a user making 2 cases.

--- classes/punish.ts
+++ classes/punish.ts
@@ -125,7 +125,7 @@ export class Punishment {
             let exists = true
             if (!modLogChannel) { exists = false; }
             if (exists == true) {
-                if (message.guild?.members.me?.permissionsIn(modLogChannel!).has([PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks])) {
+                if (message.guild?.members.me?.permissionsIn(modLogChannel!).has([PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks, PermissionsBitField.Flags.ViewChannel])) {
                     (message.guild?.channels.cache.find((c: any) => c.id === modLogChannel?.id) as TextChannel).send({ embeds: [modLogs] }).catch((err: Error) => console.error(err))
                 }
             }
@@ -243,80 +243,81 @@ export class Punishment {
                     let exists = true
                     if (!channel) { exists = false; }
                     if (exists == true) {
-                        if (message.guild?.members.me?.permissionsIn(channel!).has([PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks])) {
+                        if (message.guild?.members.me?.permissionsIn(channel!).has([PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks, PermissionsBitField.Flags.ViewChannel])) {
                             (message.guild?.channels.cache.find((c: any) => c.id === channel?.id) as TextChannel).send({ embeds: [modLogs] })
                         }
                     }
                     if (message.guild.members.me.roles.highest.position < member.roles.highest.position) return message.channel.send({ content: "I cannot mute this user!" })
-                    return member.timeout(ms("10m")).catch((err: Error) => {
+                    member.timeout(ms("10m")).catch((err: Error) => {
                         console.error(err)
                         message.channel.send({ content: "I could not mute this user!" })
                     });
-                }
+                    return;
+                } else {
 
-                const newCase = new Cases({
-                    guildID: message.guild?.id,
-                    userID: user.id,
-                    modID: message.author.id,
-                    caseType: "Warn",
-                    caseReason: reason,
-                    caseNumber: caseNumberSet,
-                    caseLength: "None",
-                    caseDate: Date.now(),
-                })
-                newCase.save().catch((err: Error) => console.error(err));
+                    const newCase = new Cases({
+                        guildID: message.guild?.id,
+                        userID: user.id,
+                        modID: message.author.id,
+                        caseType: "Warn",
+                        caseReason: reason,
+                        caseNumber: caseNumberSet,
+                        caseLength: "None",
+                        caseDate: Date.now(),
+                    })
+                    newCase.save().catch((err: Error) => console.error(err));
 
-                if (settings.modSettings?.dmOnPunish == true) {
-                    const dm = new EmbedBuilder()
-                        .setAuthor({ name: "You Were Warned in " + message.guild?.name + "!", iconURL: message.guild?.iconURL() || undefined })
-                        .setColor(color)
-                        .setDescription(`<:blurple_bulletpoint:997346294253244529> **Reason:** ${reason}
+                    if (settings.modSettings?.dmOnPunish == true) {
+                        const dm = new EmbedBuilder()
+                            .setAuthor({ name: "You Were Warned in " + message.guild?.name + "!", iconURL: message.guild?.iconURL() || undefined })
+                            .setColor(color)
+                            .setDescription(`<:blurple_bulletpoint:997346294253244529> **Reason:** ${reason}
                         <:blurple_bulletpoint:997346294253244529> **Case:** #${caseNumberSet}`)
+                            .setTimestamp()
+                        if (settings.guildSettings?.premium == false || !settings.guildSettings?.premium) {
+                            user.send({ embeds: [dm], components: [row] }).catch((err: Error) => {
+                                console.error(err)
+                            })
+                        } else if (settings.guildSettings?.premium == true) {
+                            user.send({ embeds: [dm] }).catch((err: Error) => {
+                                console.error(err)
+                            })
+                        }
+                    }
+
+                    const warned = new EmbedBuilder()
+                        .setDescription(`**Case:** #${caseNumberSet} | **Mod:** ${message.author.tag} | **Reason:** ${reason}`)
+                        .setColor(color)
+                    message.channel.send({ content: `<:arrow_right:967329549912248341> **${user.tag}** has been warned! (Warns **${warns}**)`, embeds: [warned] })
+
+                    const modLogs = new EmbedBuilder()
+                        .setAuthor({ name: `Member Warned - ${user.tag}`, iconURL: user.displayAvatarURL() || undefined })
+                        .setThumbnail(user.displayAvatarURL() || null)
+                        .setDescription(`<:user:977391493218181120> **User:** ${user.tag}
+                        > [${user.id}]
+                        > [<@${user.id}>]
+            
+                        <:folder:977391492790362173> **Mod:** ${message.author.tag}
+                        > [${message.author.id}]
+                        > [<@${message.author.id}>]
+            
+                        <:pencil:977391492916207636> **Action:** Warn
+                        > [**Case:** #${caseNumberSet}]
+            
+                        **Reason:** ${reason}
+                        **Channel:** <#${message.channel?.id}>
+                        **Date:** <t:${Math.round(Date.now() / 1000)}:D>`)
+                        .setColor(color)
                         .setTimestamp()
-                    if (settings.guildSettings?.premium == false || !settings.guildSettings?.premium) {
-                        user.send({ embeds: [dm], components: [row] }).catch((err: Error) => {
-                            console.error(err)
-                        })
-                    } else if (settings.guildSettings?.premium == true) {
-                        user.send({ embeds: [dm] }).catch((err: Error) => {
-                            console.error(err)
-                        })
+                    const channel = message.guild?.channels.cache.find((c: any) => c.id === settings.modSettings?.modLogChannel!);
+                    let exists = true
+                    if (!channel) { exists = false; }
+                    if (exists == true) {
+                        if (message.guild?.members.me?.permissionsIn(channel!).has([PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks, PermissionsBitField.Flags.ViewChannel])) {
+                            (message.guild?.channels.cache.find((c: any) => c.id === channel?.id) as TextChannel).send({ embeds: [modLogs] })
+                        }
                     }
                 }
-
-                const warned = new EmbedBuilder()
-                    .setDescription(`**Case:** #${caseNumberSet} | **Mod:** ${message.author.tag} | **Reason:** ${reason}`)
-                    .setColor(color)
-                message.channel.send({ content: `<:arrow_right:967329549912248341> **${user.tag}** has been warned! (Warns **${warns}**)`, embeds: [warned] })
-
-                const modLogs = new EmbedBuilder()
-                    .setAuthor({ name: `Member Warned - ${user.tag}`, iconURL: user.displayAvatarURL() || undefined })
-                    .setThumbnail(user.displayAvatarURL() || null)
-                    .setDescription(`<:user:977391493218181120> **User:** ${user.tag}
-                    > [${user.id}]
-                    > [<@${user.id}>]
-        
-                    <:folder:977391492790362173> **Mod:** ${message.author.tag}
-                    > [${message.author.id}]
-                    > [<@${message.author.id}>]
-        
-                    <:pencil:977391492916207636> **Action:** Warn
-                    > [**Case:** #${caseNumberSet}]
-        
-                    **Reason:** ${reason}
-                    **Channel:** <#${message.channel?.id}>
-                    **Date:** <t:${Math.round(Date.now() / 1000)}:D>`)
-                    .setColor(color)
-                    .setTimestamp()
-                const channel = message.guild?.channels.cache.find((c: any) => c.id === settings.modSettings?.modLogChannel!);
-                let exists = true
-                if (!channel) { exists = false; }
-                if (exists == true) {
-                    if (message.guild?.members.me?.permissionsIn(channel!).has([PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks])) {
-                        (message.guild?.channels.cache.find((c: any) => c.id === channel?.id) as TextChannel).send({ embeds: [modLogs] })
-                    }
-                }
-
                 break;
             case 5:
 
